3b545ecbcfb994621921de1c2b9fad7da407bf18
Maxim Aderiha <fpm.aderiha@bsu.by>
2017-12-05 02:43:46 +0300

    simple saga with test for people duck

---
+++ src/ducks/people.test.js
@@ -0,0 +1,26 @@
+import {addPersonSaga, ADD_PERSON_REQUEST, ADD_PERSON} from './people';
+import {call, put} from 'redux-saga/effects';
+import {generateId} from './utils';
+
+
+it('should dispatch person with id', () => {
+    const person = {
+        firstName: 'Maxim',
+        lastName: 'Aderiha',
+        email: 'test@text.com',
+    };
+
+    const saga = addPersonSaga({
+        type: ADD_PERSON_REQUEST,
+        payload: person,
+    });
+
+    expect(saga.next().value).toEqual(call(generateId));
+
+    const id = generateId();
+
+    expect(saga.next(id).value).toEqual(put({
+        type: ADD_PERSON,
+        payload: {...person, id},
+    }))
+});
